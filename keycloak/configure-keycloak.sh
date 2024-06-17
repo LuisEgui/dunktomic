1f339fec9d59a49f89f9d44a6799b36a6c21e291
Luis Egui <misionpage@gmail.com>
2024-06-17 08:19:04 +0200

    fix: keycloak config

--- keycloak/configure-keycloak.sh
+++ keycloak/configure-keycloak.sh
@@ -16,8 +16,6 @@ wait_for_port() {
 # Esperar a que Keycloak inicie en el puerto 8080
 wait_for_port localhost 8080
 
-/opt/keycloak/bin/kcadm.sh help config
-
 # Autenticar como administrador
 /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user $KEYCLOAK_ADMIN --password $KEYCLOAK_ADMIN_PASSWORD
 
@@ -59,14 +57,28 @@ CLIENT_CONFIG='{
   "nodeReRegistrationTimeout": 0,
   "protocolMappers": [
     {
+      "name": "Client ID",
+      "protocol": "openid-connect",
+      "protocolMapper": "oidc-usersessionmodel-note-mapper",
+      "consentRequired": false,
+      "config": {
+        "user.session.note": "client_id",
+        "id.token.claim": "true",
+        "introspection.token.claim": "true",
+        "access.token.claim": "true",
+        "claim.name": "client_id",
+        "jsonType.label": "String"
+      }
+    },
+    {
       "name": "Client Host",
       "protocol": "openid-connect",
       "protocolMapper": "oidc-usersessionmodel-note-mapper",
       "consentRequired": false,
       "config": {
         "user.session.note": "clientHost",
-        "introspection.token.claim": "true",
         "id.token.claim": "true",
+        "introspection.token.claim": "true",
         "access.token.claim": "true",
         "claim.name": "clientHost",
         "jsonType.label": "String"
@@ -79,24 +91,10 @@ CLIENT_CONFIG='{
       "consentRequired": false,
       "config": {
         "user.session.note": "clientAddress",
-        "introspection.token.claim": "true",
         "id.token.claim": "true",
+        "introspection.token.claim": "true",
         "access.token.claim": "true",
         "claim.name": "clientAddress",
-        "jsonType.label": "String"
-      }
-    },
-    {
-      "name": "Client ID",
-      "protocol": "openid-connect",
-      "protocolMapper": "oidc-usersessionmodel-note-mapper",
-      "consentRequired": false,
-      "config": {
-        "user.session.note": "client_id",
-        "introspection.token.claim": "true",
-        "id.token.claim": "true",
-        "access.token.claim": "true",
-        "claim.name": "client_id",
         "jsonType.label": "String"
       }
     }
@@ -106,6 +104,7 @@ CLIENT_CONFIG='{
     "acr",
     "profile",
     "roles",
+    "basic",
     "email"
   ],
   "optionalClientScopes": [
@@ -125,14 +124,67 @@ CLIENT_CONFIG='{
 echo "$CLIENT_CONFIG" > /tmp/client-config.json
 
 # Obtener el ID del cliente "admin-cli"
-CLIENT_ID=$(/opt/keycloak/bin/kcadm.sh get clients -r $REALM | grep -A 10 -B 10 '"clientId" : "admin-cli"' | grep '"id" :' | sed -E 's/.*"id" : "([^"]+)".*/\1/')
+CLIENT_ID=$(/opt/keycloak/bin/kcadm.sh get clients --realm master | grep -A 10 -B 10 '"clientId" : "admin-cli"' | grep '"id"' | head -n 1 | sed -E 's/.*"id" : "([^"]+)".*/\1/')
+echo "CLIENT_ID: $CLIENT_ID"
 
 # Actualizar el cliente con la configuración especificada
-/opt/keycloak/bin/kcadm.sh update clients/$CLIENT_ID -r $REALM -f /tmp/client-config.json
+/opt/keycloak/bin/kcadm.sh update clients/$CLIENT_ID --realm master -f /tmp/client-config.json
+
+# Crear el rol manage-users para el cliente admin-cli
+/opt/keycloak/bin/kcadm.sh create clients/$CLIENT_ID/roles --realm master -s name=manage-users -s description="Role to manage users"
+
+# Obtener el ID del rol manage-users del cliente admin-cli
+CLIENT_ROLE_SEARCH_RESULT=$(/opt/keycloak/bin/kcadm.sh get clients/$CLIENT_ID/roles --realm master | grep -A 10 -B 10 '"name" : "manage-users"' | grep '"id" :' | head -n 1 | sed -E 's/.*"id" : "([^"]+)".*/\1/')
+echo "CLIENT_ROLE_ID: $CLIENT_ROLE_SEARCH_RESULT"
+
+# Obtener el ID del rol manage-users del cliente master-realm utilizando el endpoint correcto
+ROLE_SEARCH_RESULT=$(/opt/keycloak/bin/kcadm.sh get ui-ext/available-roles/roles/$CLIENT_ROLE_SEARCH_RESULT -r master -q search=manage-users)
+REALM_ROLE_ID=$(echo "$ROLE_SEARCH_RESULT" | grep '"id" :' | sed -E 's/.*"id" : "([^"]+)".*/\1/')
+echo "REALM_ROLE_ID: $REALM_ROLE_ID"
+
+# Construir el objeto JSON para la asociación de roles, asegurando que los caracteres se escapen correctamente
+COMPOSITE_ROLE_JSON=$(cat <<EOF
+[
+  {
+    "id": "$REALM_ROLE_ID",
+    "name": "manage-users",
+    "description": "\${role_manage-users}"
+  }
+]
+EOF
+)
+
+# Verificar que el JSON es correcto
+echo "COMPOSITE_ROLE_JSON: $COMPOSITE_ROLE_JSON"
+
+# Asociar el rol manage-users del realm master al rol manage-users del cliente admin-cli
+echo "$COMPOSITE_ROLE_JSON" | /opt/keycloak/bin/kcadm.sh create roles-by-id/$CLIENT_ROLE_SEARCH_RESULT/composites --realm master -f -
+
+# Asociar el rol manage-users del cliente admin-cli al rol manage-users del realm master
+echo "$COMPOSITE_ROLE_JSON" | /opt/keycloak/bin/kcadm.sh create roles-by-id/$REALM_ROLE_ID/composites --realm master -f -
+
+# Obtener el ID del service account del cliente admin-cli
+SERVICE_ACCOUNT_JSON=$(/opt/keycloak/bin/kcadm.sh get clients/$CLIENT_ID/service-account-user --realm master)
+SERVICE_ACCOUNT_ID=$(echo "$SERVICE_ACCOUNT_JSON" | grep '"id"' | head -n 1 | sed -E 's/.*"id" : "([^"]+)".*/\1/')
+echo "SERVICE_ACCOUNT_ID: $SERVICE_ACCOUNT_ID"
+
+SEVICE_ACCOUNT_ROLE_JSON=$(cat <<EOF
+[
+  {
+    "id": "$CLIENT_ROLE_SEARCH_RESULT",
+    "name": "manage-users",
+    "description": "\${role_manage-users}"
+  }
+]
+EOF
+)
+
+echo "SEVICE_ACCOUNT_ROLE_JSON: $SEVICE_ACCOUNT_ROLE_JSON"
+
+echo "$SEVICE_ACCOUNT_ROLE_JSON" | /opt/keycloak/bin/kcadm.sh create users/$SERVICE_ACCOUNT_ID/role-mappings/clients/$CLIENT_ID --realm master -f -
 
 # Logout (eliminar la configuración de credenciales)
-#/opt/keycloak/bin/kcadm.sh config delete
-#rm ~/.keycloak/kcadm.config
+rm ~/.keycloak/kcadm.config
 
 # Mantener el contenedor en ejecución
 tail -f /dev/null

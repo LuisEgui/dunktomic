9e11c012141a4738acd7d0a9c4719975a5182b9b
Luis Egui <misionpage@gmail.com>
2024-06-19 06:28:28 +0200

    refactor(frontend): removed auth-service
    included local storage constant "user_id"

--- frontend/code/src/services/api/players.js
+++ frontend/code/src/services/api/players.js
@@ -1,6 +1,6 @@
 import { http } from "../http";
 import axios from "axios";
-import { localStorageService } from "../local-storage";
+import { LOCAL_STORAGE_KEYS, localStorageService } from "../local-storage";
 
 export const register = async ({ name, email, password, positions_on_court }) => {
   const response = await http().post('/players', {
@@ -11,6 +11,6 @@ export const register = async ({ name, email, password, positions_on_court }) =>
   });
 
   const userId = response.headers['x-resource-id'];
-  localStorageService.setItem('user-id', userId);
+  localStorageService.setItem(LOCAL_STORAGE_KEYS.USER_ID, userId);
   return { id: userId, status: response.status };
 };
